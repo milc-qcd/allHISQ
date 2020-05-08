835bd1ecdab2b610ea1528701dca7dcd4cb01da4
Carleton DeTar <detar@physics.utah.edu>
2020-05-08 10:02:06 -0700

    Purge under run* instead of run?

--- l64144f211b672m0024m024m286/purge_corrs.sh
+++ l64144f211b672m0024m024m286/purge_corrs.sh
@@ -2,7 +2,7 @@
 
 # Remove any prior stray corrs
 
-# Usage ./purge_corrs.sh <LATS> <NCASES>
+# Usage ./purge_corrs.sh <LATS> [ <NCASES> ]
 
 LATS=$1
 NCASES=$2
@@ -16,5 +16,5 @@ cfgs_milc=( `echo ${LATS} | sed 's|/| |g'` )
 for((i=0; i<${NCASES}; i++)); do
   cfg_fnal=`echo ${cfgs_milc[$i]} | awk -F. '{printf("%s%06d",$1,$2)}'`
   echo "Purging corrs for ${cfg_fnal}"
-  find run?/data -name '*'${cfg_fnal} -exec /bin/rm '{}' \;
+  find run*/data -name '*'${cfg_fnal} -exec /bin/rm '{}' \;
 done
