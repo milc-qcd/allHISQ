18db95f2687711068d936c7c78cce09cf96a1e2c
Carleton DeTar <detar@physics.utah.edu>
2020-09-02 05:24:41 -0700

    Be more precise in selecting run directories

--- scripts/list_tar_cfgs.sh
+++ scripts/list_tar_cfgs.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-for d in run*
+for d in run3?
 do
     echo $d
-    ls -1 $d/Job* | awk -F_ '{print $2}' | awk -F. '{print $1}' | sort | uniq -c > $d/foo
+    ls -1 $d/tar/Job* | awk -F_ '{print $2}' | awk -F. '{print $1}' | sort | uniq -c > $d/tar/foo
 done
+
+wc -l run3?/tar/foo
 
