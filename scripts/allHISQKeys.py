9366143f330ef2a2495cd1c655d7cf74cd93f652
wijay <william.jay@colorado.edu>
2021-08-16 14:00:51 -0400

    Update for USQCD 2021 Running

--- scripts/allHISQKeys.py
+++ scripts/allHISQKeys.py
@@ -3,6 +3,16 @@
 # C DeTar 5 April 2018
 
 ######################################################################
+def makeTwistKey(twist):
+    """Create momentum key"""
+    return "{0:d},{1:d},{2:d}".format(*tuple(twist))
+
+######################################################################
+def splitTwistKey(twistKey):
+    """Split momentum key"""
+    return [int(val) for val in twistKey.split(",")]
+
+######################################################################
 def makeMomKey(mom):
     """Create momentum key"""
     return "{0:d},{1:d},{2:d}".format(*tuple(mom))
@@ -49,30 +59,39 @@ def splitQuarkKey(qkKey):
 ######################################################################
 def cmpQuarkKeys(qkKey1, qkKey2):
     """Compare quark keys"""
-    # We must put the point source first 
-    (rq1, qk1, m1, e1, r1, srcKey1, snk1) = splitQuarkKey(qkKey1)
-    (rq2, qk2, m2, e2, r2, srcKey2, skn2) = splitQuarkKey(qkKey2)
-    (src1, mom1) = splitSrcKey(srcKey1)
-    (src2, mom2) = splitSrcKey(srcKey2)
+    # We must put the point source first
+    srcKey1 = splitQuarkKey(qkKey1)[5]  # (rq1, qk1, m1, e1, r1, srcKey1, snk1, twist)
+    srcKey2 = splitQuarkKey(qkKey2)[5]  # similar to previous
+    src1, _ = splitSrcKey(srcKey1)
+    src2, _ = splitSrcKey(srcKey2)
+    # (rq1, qk1, m1, e1, r1, srcKey1, snk1, twist1) = splitQuarkKey(qkKey1)
+    # (rq2, qk2, m2, e2, r2, srcKey2, skn2, twist2) = splitQuarkKey(qkKey2)
+    # (src1, mom1) = splitSrcKey(srcKey1)
+    # (src2, mom2) = splitSrcKey(srcKey2)
     if src1 == 'd':
         order = -1
         if src2 == 'd':
-            order = (qkKey1 > qkKey2) - (qkKey2 < qkKey1) 
+            order = (qkKey1 > qkKey2) - (qkKey2 < qkKey1)
     elif src2 == 'd':
         order = +1
     else:
-        order = (qkKey1 > qkKey2) - (qkKey2 < qkKey1) 
+        order = (qkKey1 > qkKey2) - (qkKey2 < qkKey1)
 
     return order
 
 ######################################################################
 def cmpQuarkKeys2(qkKey1, qkKey2):
     """Compare quark keys.  This order is for multimass KS"""
-
-    (rq1, qk1, m1, e1, r1, srcKey1, snk1) = splitQuarkKey(qkKey1)
-    (rq2, qk2, m2, e2, r2, srcKey2, skn2) = splitQuarkKey(qkKey2)
-    (src1, mom1) = splitSrcKey(srcKey1)
-    (src2, mom2) = splitSrcKey(srcKey2)
+    tokens1 = splitQuarkKey(qkKey1)  # (rq1, qk1, m1, e1, r1, srcKey1, snk1, twist)
+    tokens2 = splitQuarkKey(qkKey2)  # similar to tokens1
+    m1 = tokens1[2]
+    m2 = tokens2[2]
+    srcKey1 = tokens1[5]
+    srcKey2 = tokens2[5]
+    # (rq1, qk1, m1, e1, r1, srcKey1, snk1) = splitQuarkKey(qkKey1)
+    # (rq2, qk2, m2, e2, r2, srcKey2, skn2) = splitQuarkKey(qkKey2)
+    # (src1, mom1) = splitSrcKey(srcKey1)
+    # (src2, mom2) = splitSrcKey(srcKey2)
 
     # Sort first on source and momentum, then on mass
     order = (srcKey1 > srcKey2) - (srcKey1 < srcKey2)
