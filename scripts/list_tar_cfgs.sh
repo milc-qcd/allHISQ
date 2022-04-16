f185f950d5e44e20de9a86c211203d88c8de7d08
Carleton DeTar <detar@physics.utah.edu>
2022-04-16 13:26:17 -0500

    Change run directory

--- scripts/list_tar_cfgs.sh
+++ scripts/list_tar_cfgs.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-for d in run3?
+for d in run2?
 do
     echo $d
     ls -1 $d/tar/Job* | awk -F_ '{print $2}' | awk -F. '{print $1}' | sort | uniq -c > $d/tar/foo
 done
 
-wc -l run3?/tar/foo
+wc -l run2?/tar/foo
 
